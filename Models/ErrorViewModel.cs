1d312af4cc76b8a45e1603bf7af0ff42c2a667c5
Michal Malujlo <michal@ipulse.systems>
2022-04-10 20:06:27 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Platform45_MarsRoverChallenge_Michal_Malujlo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
